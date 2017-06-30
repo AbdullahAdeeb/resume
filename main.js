b9d52d8c6fd8268a310a7f04ea59bc946eb6535b
Abdullah Adeeb <contact@abdullahadeeb.com>
2017-06-30 01:03:05 -0400

    adding node server

--- main.js
+++ main.js
@@ -3,41 +3,45 @@ window.onload = function() {
     var a = document.getElementById("skills.svg");
     // Get the SVG document inside the Object tag
     var $svg = $(a.contentDocument);
-	$svg.find("path").css("pointer-events", "none");
+    $svg.find("path").css("pointer-events", "none");
 
-    
-	var outer = $svg.find("path[fill='#b4f1c5']");
-    outer.css("transform-origin","50% 50%");
-	outer.css("pointer-events", "auto");
-    outer.hover(function(){
-        outer.css("fill","grey");
-        outer.css("stroke","black");
-    },function(){
-        outer.css("fill","#b4f1c5");
-        outer.css("stroke","none");
+
+    var outer = $svg.find("path[fill='#efefef']");
+    outer.css("transform-origin", "50% 50%");
+    outer.css("pointer-events", "auto");
+    outer.hover(function() {
+        outer.css("fill", "grey");
+        outer.css("stroke", "black");
+    }, function() {
+        outer.css("fill", "#efefef");
+        outer.css("stroke", "none");
     });
-	
-	
-	var mid = $svg.find("path[fill='#92cbc4']");
-    mid.css("transform-origin","50% 50%");
-	mid.css("pointer-events", "auto");
-    mid.hover(function(){
-        mid.css("fill","grey");
-        mid.css("stroke","black");
-    },function(){
-        mid.css("fill","#92cbc4");
-        mid.css("stroke","none");
+
+
+    var mid = $svg.find("path[fill='#8dd8bc']");
+    mid.css("transform-origin", "50% 50%");
+    mid.css("pointer-events", "auto");
+    mid.hover(function() {
+        mid.css("fill", "grey");
+        mid.css("stroke", "black");
+    }, function() {
+        mid.css("fill", "#8dd8bc");
+        mid.css("stroke", "none");
     });
-	
-	
-	var inner = $svg.find("path[fill='#7b93a4']");
-    inner.css("transform-origin","50% 50%");
-	inner.css("pointer-events", "auto");
-    inner.hover(function(){
-        inner.css("fill","grey");
-        inner.css("stroke","black");
-    },function(){
-        inner.css("fill","#7b93a4");
-        inner.css("stroke","none");
+
+
+    var inner = $svg.find("path[fill='#45818e']");
+    inner.css("transform-origin", "50% 50%");
+    inner.css("pointer-events", "auto");
+    inner.hover(function() {
+        inner.css("fill", "grey");
+        inner.css("stroke", "black");
+    }, function() {
+        inner.css("fill", "#45818e");
+        inner.css("stroke", "none");
     });
+}
+
+function onPrint() {
+    window.print();
 }
